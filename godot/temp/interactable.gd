d730549e871813c15d81da3da6f204ff256fe212
lucas-d00 <reposnate1738@gmail.com>
2025-04-11 03:28:01 -0300

    Added Interactable + InteractRay behavior in test_player.gd

---
+++ godot/temp/interactable.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+@export var message = 'Texto de interacción'
+var interaction_enabled = false
+
+func _ready():
+	$PopUp/Label.text = message + '\n(Presione X para cerrar)'
+
+func display_message():
+	$PopUp.visible = true
+
+func hide_message():
+	$PopUp.visible = false
+
+func enable_interaction():
+	interaction_enabled = true
+
+func disable_interaction():
+	interaction_enabled = false
+
+func handle_interaction():
+	if (interaction_enabled):
+		display_message()
+	else:
+		hide_message()
+	
+	if (Input.is_key_pressed(KEY_X)):
+		disable_interaction()
+
+func _process(delta):
+	handle_interaction()
--- godot/temp/test_player.gd
+++ godot/temp/test_player.gd
@@ -6,19 +6,39 @@ extends CharacterBody3D
 @onready var head: Node3D = %Head
 @onready var player_camera: Camera3D = %PlayerCamera
 @onready var interact_ray: RayCast3D = %InteractRay
+var rayCollision = false #es true Si InteractRay del player colisiona con un área o body
 
+func check_ray_collision() -> void:
+	var collider = %InteractRay.get_collider()
+	#Chequea las colisiones del InteractRay del Player
+	if(%InteractRay.is_colliding()):
+		if(collider.get_name() == 'Interactable'):
+			#Si es un objeto interactuable despliega el menú de contexto
+			$Context.text = 'Interactuar: (E)'
+			$Context.visible = true
+			#Al presionar E se interactúa con el objeto
+			if(Input.is_key_pressed(KEY_E)):
+				collider.enable_interaction()
+	else:
+		$Context.visible = false
+		
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-
 func _process(delta: float) -> void:
 	var move_vector := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var camera_orientation := player_camera.basis
+	var camera_orientation := (transform.basis * Vector3(move_vector.x, 0, move_vector.y)).normalized()
 	
-	velocity = Vector3(move_vector.x, 0.0, move_vector.y) * SPEED * delta
+	if camera_orientation:
+		velocity.x = camera_orientation.x * SPEED
+		velocity.z = camera_orientation.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	move_and_slide()
+	check_ray_collision()
 
 
 func _input(event: InputEvent) -> void:
